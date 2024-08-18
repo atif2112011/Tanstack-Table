0c6968d86de084fbbc0cc7c1f19c988df6867c45
Blaze1012 <atif251171@gmail.com>
2024-08-18 19:39:40 +0530

    search filter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SearchIcon from '../assets/search.svg'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 function Search({columnFilters,setColumnFilters}) {
+
+    const navigate=useNavigate();
+    const [searchParams,setSearchParams]=useSearchParams();
+
+
+    const currentFilter=searchParams.get(`query`) || columnFilters.find(f=>f.id==='name')?.value ||"";
 
     const onFilterChange=(id,value)=>{
         setColumnFilters(
@@ -8,9 +15,20 @@ function Search({columnFilters,setColumnFilters}) {
                 id,value
             })
         )
+
+        if (value) {
+            setSearchParams({ query: value });
+            navigate(`/people?query=${value}`);
+          } else {
+            setSearchParams({});
+            navigate('/people');
+          }
     }
 
-    const currentFilter=columnFilters.find(f=>f.id=='name')?.value || "";
+    useEffect(() => {
+        onFilterChange('name', currentFilter);
+      }, []);
+
   return (
     <div className='flex border border-gray-300 bg-white rounded-md px-2 py-1 items-center'>
         <input className='w-[300px] items-center text-sm ' type='text' placeholder='Search' value={currentFilter} onChange={(e)=>onFilterChange('name',e.target.value)}></input>
